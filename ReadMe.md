a6b9c3fce84fef878ab2ca360ba7533d41d300c1
Saad Baig <anothersaadbaig@gmail.com>
2024-06-03 23:20:36 +0500

    a

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+# Project Setup
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your `.env` file
+
+MONGODB_PASSWORD=yourpassword
+MONGODB_USER=yourusername
--- api/Schemas/db.js
+++ api/Schemas/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const ConnecitontoDatabase = ()=>{
 
-  mongoose.connect(`mongodb+srv://saadb451:${process.env.DBPASSWORD}!@cluster0.vbcrt.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+  mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.vbcrt.mongodb.net/?retryWrites=true&w=majority`).then(() => {
     console.log("connected")
   }).catch((error) => {
     console.log(error)
